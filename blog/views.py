a64344b44660c1760accbc90cc776f889d3a019e
Gxstar <gengxing123@qq.com>
2020-05-05 17:12:54 +0800

    静态文件改用cdn

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from .models import Category,Tag,Article
 # Create your views here.
+def home(request):
+    AllArticle=Article.objects.all()
+    AllCategory=Category.objects.all()
+    context={
+        'ArticleList':AllArticle,
+        'CategoryList':AllCategory
+    }
+    if request.method=='GET':
+        return render(request,'blog/index.html',context)
+    else:
+        pass
+    
