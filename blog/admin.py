ee004f322f3fbdbf8629682de05953af10a46d1a
Gxstar <gengxing123@qq.com>
2020-05-11 22:26:19 +0800

    准备页码设置

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from .models import Category,Tag,Article
 # Register your models here.
+class ArticleAdmin(admin.ModelAdmin):
+    list_display=('title','createTime','updateTime')
+
 admin.site.register(Category)
 admin.site.register(Tag)
-admin.site.register(Article)
+admin.site.register(Article,ArticleAdmin)
--- blog/views.py
+++ blog/views.py
@@ -2,13 +2,22 @@ from django.shortcuts import render
 from .models import Category,Tag,Article
 # Create your views here.
 def home(request):
-    AllArticle=Article.objects.all()
     AllCategory=Category.objects.all()
     context={
-        'ArticleList':AllArticle,
         'CategoryList':AllCategory
     }
     if request.method=='GET':
+        page=request.GET.get('page')
+        if page==None:
+            page=1
+        # 判断页码参数是否可以转化为整数
+        elif page.isnumeric():
+            page=int(page)
+        else:
+            pass
+        PostCount=Article.objects.count()
+        PageCount=int(PostCount/5)
+        ArticleList=Article.objects.all()[(0+5*(page-1)):(4+5*(page-1))]
         return render(request,'blog/index.html',context)
     else:
         pass
