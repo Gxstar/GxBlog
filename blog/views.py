8f231a8c9bcc581bbd757b6ac762a40ca98c00eb
Gxstar <gengxing123@qq.com>
2020-05-14 00:07:15 +0800

    调整网页结构

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Category,Tag,Article
+from bs4 import BeautifulSoup
 # Create your views here.
 def home(request):
     AllCategory=Category.objects.all()
@@ -18,6 +19,16 @@ def home(request):
         PostCount=Article.objects.count()
         PageCount=int(PostCount/5)
         ArticleList=Article.objects.all()[(0+5*(page-1)):(4+5*(page-1))]
+        # 提取文章前128个字节作为简介
+        for index,val in enumerate(ArticleList):
+            bs=BeautifulSoup(val.body,"lxml")
+            i=bs.get_text().strip()[0:255]
+            ArticleList[index].info=i
+        # 给需要的参数赋值
+        context['ArticleList']=ArticleList
+        context['PostCount']=PostCount
+        context['PageCount']=PageCount
+        context['ActivePage']=page
         return render(request,'blog/index.html',context)
     else:
         pass
