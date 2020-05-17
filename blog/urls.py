33e6ded7704300cf0c4275e514319c02283428d7
Gxstar <gengxing123@qq.com>
2020-05-17 22:56:55 +0800

    完善一下侧边栏

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,8 @@
-from django.urls import path
+from django.urls import path,re_path
+from django.contrib.auth.views import LogoutView
 from . import views
 
 urlpatterns = [
     path('', views.home,name='BlogHome'),
+    re_path(r'^(.*)logout/$',LogoutView.as_view(next_page='/blog/'), name='logout'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -4,9 +4,13 @@ from bs4 import BeautifulSoup
 # Create your views here.
 def home(request):
     AllCategory=Category.objects.all()
+    PostCount=Article.objects.count()
+    PageCount=int(PostCount/5)
     context={
         'CategoryList':AllCategory
     }
+    context['PostCount']=PostCount
+    context['PageCount']=PageCount
     if request.method=='GET':
         page=request.GET.get('page')
         if page==None:
@@ -16,8 +20,6 @@ def home(request):
             page=int(page)
         else:
             pass
-        PostCount=Article.objects.count()
-        PageCount=int(PostCount/5)
         ArticleList=Article.objects.all()[(0+5*(page-1)):(4+5*(page-1))]
         # 提取文章前128个字节作为简介
         for index,val in enumerate(ArticleList):
@@ -26,8 +28,6 @@ def home(request):
             ArticleList[index].info=i
         # 给需要的参数赋值
         context['ArticleList']=ArticleList
-        context['PostCount']=PostCount
-        context['PageCount']=PageCount
         context['ActivePage']=page
         return render(request,'blog/index.html',context)
     else:
